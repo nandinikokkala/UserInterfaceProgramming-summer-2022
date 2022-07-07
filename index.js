d372a7c05c349909529fdeca7bcd8e7a274fb397
nandinikokkala <kokkalan1@newpaltz.edu>
2022-07-07 23:17:20 +0300

    Node server

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); 
 app.use('/api/users', usersController);
 app.use('/api/posts', postsController);
-const SERVERPORT = process.env.PORT || 3000;
+const SERVERPORT = process.env.PORT || 5000;
 app.listen(SERVERPORT, ()=> console.log(`Server started on port ${SERVERPORT}`));
